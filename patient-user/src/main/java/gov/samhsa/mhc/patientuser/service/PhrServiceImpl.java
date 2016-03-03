86de7a44a6210139da0eb06687bb53ab92b4e275
burcakulug <burcak.ulug@feisystems.com>
2016-03-03 09:54:39 -0500

    Return 404 not found response code when the patient is not found on PHR
    #8198:As a developer, I will be able to utilize an API for creating and activating a patient account

--- patient-user/src/main/java/gov/samhsa/mhc/patientuser/service/PhrServiceImpl.java
+++ patient-user/src/main/java/gov/samhsa/mhc/patientuser/service/PhrServiceImpl.java
@@ -2,10 +2,13 @@ package gov.samhsa.mhc.patientuser.service;
 
 import gov.samhsa.mhc.common.oauth2.RestTemplateOAuth2TokenHelper;
 import gov.samhsa.mhc.patientuser.service.dto.PatientDto;
+import gov.samhsa.mhc.patientuser.service.exception.PhrPatientNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -20,11 +23,18 @@ public class PhrServiceImpl implements PhrService {
     @Override
     public PatientDto findPatientProfileById(OAuth2Authentication oAuth2Authentication, Long patientId) {
         final String url = toPatientProfileUri(patientId);
-        final PatientDto patientDto = RestTemplateOAuth2TokenHelper.getWithToken(restTemplate, oAuth2Authentication, url, PatientDto.class);
-        return patientDto;
+        try {
+            final PatientDto patientDto = RestTemplateOAuth2TokenHelper.getWithToken(restTemplate, oAuth2Authentication, url, PatientDto.class);
+            return patientDto;
+        } catch (HttpClientErrorException e) {
+            if (e.getStatusCode().equals(HttpStatus.NOT_FOUND)) {
+                e = new PhrPatientNotFoundException(e.getStatusCode(), e.getStatusText(), e.getResponseHeaders(), e.getResponseBodyAsByteArray(), null);
+            }
+            throw e;
+        }
     }
 
-    private final String toPatientProfileUri(Long patientId){
+    private final String toPatientProfileUri(Long patientId) {
         return phrApiBaseUri + "/patients/" + patientId + "/profile";
     }
 }
